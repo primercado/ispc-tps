8b7a2fbf91334a5fce5275119fabedc077a58d9b
primercado <primercado90@gmail.com>
2022-05-21 01:06:26 -0300

    Consignas

--- primer_tp.py
+++ primer_tp.py
@@ -1,1 +1,11 @@
-#primer tp Instituto Superior Politécnico de Cordoba
+#ISPC: Curso Programación: Primer TP
+
+# Crear un programa en Python que tendrá 3 o 4 funciones:
+#  Suma, 3 parámetros, devuelve la suma de los 3.
+#  Resta, 2 parámetros, devuelve la resta de los 2.
+#  Producto, 4 parámetros, devuelve el producto de los 4.
+#  Imprimir, 2 parámetros, texto y valor, devuelve la impresión de los valores pasados.
+# Usaremos Visual Studio Code, con la extensión de Python para hacer la práctica.
+# El equipo crea un Project en Githug y agrega 2 al menos 2 issue por cada tarea a realizar.
+# Crear una rama por cada miembro.
+# El primer integrante del grupo, creará el repositorio, creará un programa en Python con la función suma, y el main y subirá al repo el código producido
